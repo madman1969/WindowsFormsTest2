2f9522c5052510b8f6b7b1c3d1573b8cb211f371
madman1969 <aross69@gmail.com>
2020-05-13 22:36:53 +0100

    Initial commit of source files

---
+++ WindowsFormsTest2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsTest2
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // ReSharper disable once HeapView.ObjectAllocation.Evident
+            Application.Run(new Form1());
+        }
+    }
+}
